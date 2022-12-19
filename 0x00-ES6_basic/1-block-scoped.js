af5b5a7c0301b5a266b755f781cf187d2bcb2dc2
Solomon Kebede <solomonaau188@gmail.com>
2022-12-19 18:40:25 +0000

    Task 1. Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,14 @@
+// eslint-disable-next-line no-unused-vars
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
   }
 
   return [task, task2];
 }
-
