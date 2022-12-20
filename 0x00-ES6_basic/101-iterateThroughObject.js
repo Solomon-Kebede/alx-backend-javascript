7bc518fbfcc99c5aaa2b3d27d570f37cd7155201
Solomon Kebede <solomonaau188@gmail.com>
2022-12-20 05:05:23 +0000

    Task 14. Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,8 @@
- export default function iterateThroughObject(reportWithIterator) {
-
- }
+export default function iterateThroughObject(reportWithIterator) {
+  let concated = ""; let result = "";
+  for (let i = 0; i < reportWithIterator.length; i++) {
+    concated = concated.concat(" | ", reportWithIterator[i]);
+  }
+  result = concated.slice(3);
+  return result;
+}
