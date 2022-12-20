fd896dcd5a0ca2bbbcfde9055b3c2a7b4958cd94
Solomon Kebede <solomonaau188@gmail.com>
2022-12-20 03:27:08 +0000

    Task 13. Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,8 @@
 export default function createIteratorObject(report) {
-
+  let iterator = []
+  for (const x of Object.keys(report.allEmployees)) {
+    iterator = [...iterator, ...(report.allEmployees[x])];
+  }
+  return iterator;
 }
 
