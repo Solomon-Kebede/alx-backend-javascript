32cfd11dcf05e038a833d49b66fd938a30357acd
Solomon Kebede <solomonaau188@gmail.com>
2023-02-21 22:22:41 +0000

    Task 2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,20 +1,32 @@
 #!/usr/bin/node
 
+const fs = require('fs');
+
 module.exports = function countStudents(path) {
-  const fs = require('fs');
   if (fs.existsSync(path) !== true) {
-	  throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
   const data = fs.readFileSync(path,
-	  {encoding:'utf8', flag:'r'});
+    { encoding: 'utf8', flag: 'r' });
   const lines = data.split('\n').slice(1, data.split('\n').length - 1);
   const fields = {};
-  let count = 0
+  let count = 0;
   lines.forEach((element) => {
-    count++;
+    count += 1;
     const elemArray = element.split(',');
-    //fields.push(elemArray[elemArray.length - 1]);
+    const fieldValue = elemArray[elemArray.length - 1];
+    const firstName = elemArray[0];
+    if (fields[fieldValue] === undefined) {
+      fields[fieldValue] = [];
+      fields[fieldValue].push(firstName);
+    } else {
+      fields[fieldValue].push(firstName);
+    }
   });
   console.log(`Number of students: ${count}`);
-  //console.log(data);
-}
+  // console.log(fields);
+  for (const [key, value] of Object.entries(fields)) {
+    console.log(`Number of students in ${key}: ${value.length}. List: ${value}`);
+  }
+  // console.log(data);
+};
