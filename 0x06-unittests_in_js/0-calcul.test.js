2365b5ce80076cd86f5aa24291a4293a5b35cd8e
Solomon Kebede <solomonaau188@gmail.com>
2023-02-22 23:10:03 +0000

    Task 0 - Basic test

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
+assert.equal(calculateNumber(1, 3), 4);
+assert.equal(calculateNumber(1, 3.7), 5);
+assert.equal(calculateNumber(1.2, 3.7), 5);
+assert.equal(calculateNumber(1.5, 3.7), 6);
