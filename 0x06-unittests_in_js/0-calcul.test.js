d5a7589272b036f841e899731b7663039cabb047
Solomon Kebede <solomonaau188@gmail.com>
2023-02-23 19:52:14 +0000

    strictEqual

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,7 @@
 
 const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
-assert.equal(calculateNumber(1, 3), 4);
-assert.equal(calculateNumber(1, 3.7), 5);
-assert.equal(calculateNumber(1.2, 3.7), 5);
-assert.equal(calculateNumber(1.5, 3.7), 6);
+assert.strictEqual(calculateNumber(1, 3), 4);
+assert.strictEqual(calculateNumber(1, 3.7), 5);
+assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+assert.strictEqual(calculateNumber(1.5, 3.7), 6);
