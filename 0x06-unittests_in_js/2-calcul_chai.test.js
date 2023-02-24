ce119cdd18ed94263b0720896abfda83404e8051
Solomon Kebede <solomonaau188@gmail.com>
2023-02-24 22:34:58 +0000

    Task 2 - Chai

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const assert = require('assert');
+const calculateNumber = require("./2-calcul_chai.js");
+
+expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
