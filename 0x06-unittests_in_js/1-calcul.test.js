d05209e45accc2aefbbb1ad56788ae3d2413207b
Solomon Kebede <solomonaau188@gmail.com>
2023-02-24 21:50:43 +0000

    Task 1

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const assert = require('assert');
+const calculateNumber = require("./1-calcul.js");
+assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6)
+assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
+assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2)
+assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error')
