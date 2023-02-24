768855811e7a72152e2b89ad6941fbfec8fc0de1
Solomon Kebede <solomonaau188@gmail.com>
2023-02-24 12:47:25 +0000

    Task 6 - express server

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,12 +3,9 @@
 const express = require('express');
 
 const app = express();
-const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port, () => {
-  // console.log(`Example app listening on port ${port}`);
-});
+app.listen(1245);
