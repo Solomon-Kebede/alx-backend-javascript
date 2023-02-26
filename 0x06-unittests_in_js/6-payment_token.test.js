be9afeaaf95f2eefd8570f85094e669bb48a66dd
Solomon Kebede <solomonaau188@gmail.com>
2023-02-26 21:28:58 +0000

    Task 6. Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,25 @@
-const sinon = require('sinon')
-const sendPaymentToApi= require("./6-payment_token.js");
+//const sinon = require('sinon')
+const getPaymentTokenFromAPI = require("./6-payment_token.js");
 const chai = require('chai');
 const expect = chai.expect;
-const jest = require("jest");
+//const jest = require("jest");
 
 describe("getPaymentTokenFromAPI", () => {
-  test('the data is peanut butter', () => {
-    return getPaymentTokenFromAPI.then(data => {
+  it('test success', () => {
+	  /*
+    return getPaymentTokenFromAPI(true).then(data => {
       expect(data).toBe('Successful response from the API');
+      */
+    const a = getPaymentTokenFromAPI(true);
+    a.then((value) => {
+      expect(value.data).to.equal("Successful response from the API");
     });
   });
+  /*
+  it('test fail', () => {
+    const a = getPaymentTokenFromAPI(true);
+    a.then((value) => {
+      expect(value).to.equal(undefined);
+  });
+  */
 });
