2c32e1b74b66a712c0224ed3b56e0752693c0326
Solomon Kebede <solomonaau188@gmail.com>
2023-03-25 06:29:08 -0700

    Task 3 - return promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then(([result1, result2]) => {
-    // console.log(result1);
-    // console.log(result2);
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then(([result1, result2]) => {
     console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
   });
 }
