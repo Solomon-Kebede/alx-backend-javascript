fdfffafb830174dc995edc43ba3780a297e6494a
Solomon Kebede <solomonaau188@gmail.com>
2023-03-25 06:33:20 -0700

    Task 3 - catch

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then(([result1, result2]) => {
     console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
-  });
+  }).catch(() => console.log('Signup system offline'));
 }
 
 module.exports = handleProfileSignup;
