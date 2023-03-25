849f920ca3a01b4825862dc1707c2b9fef1c5474
Solomon Kebede <solomonaau188@gmail.com>
2023-03-25 04:18:34 -0700

    Task 2. Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then((resolve) => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  },
-  (reject) => {
-    Error();
+  return promise.then((result) => {
+      console.log('Got a response from the API');
+      return {status: 200, body: 'success'};
+  }, (error) => {
+      return new Error();
   });
 }
