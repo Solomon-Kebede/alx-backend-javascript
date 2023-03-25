8105b813f8dadde5d13c9ab39de567712ee5ee75
Solomon Kebede <solomonaau188@gmail.com>
2023-03-25 11:18:20 -0700

    Task 9. Throw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  let queue;
+  try {
+    const result = mathFunction();
+    queue = [result, 'Guardrail was processed'];
+    return queue;
+  }
+  catch (err) {
+    queue = [err, 'Guardrail was processed'];
+    return queue;
+  }
+}
