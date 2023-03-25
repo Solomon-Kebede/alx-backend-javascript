8e7826418902d95c2e79118734cf185643b38481
Solomon Kebede <solomonaau188@gmail.com>
2023-03-25 05:47:15 -0700

    Task 2 - eslint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then((result) => {
-      console.log('Got a response from the API');
-      return {status: 200, body: 'success'};
-  }, (error) => {
-      return new Error();
-  });
+  return promise.then((error) => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  }, (error) => new Error());
 }
