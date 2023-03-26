5e0efd0634942aa4e385ff257ff8b052691a538e
Solomon Kebede <solomonaau188@gmail.com>
2023-03-25 17:56:48 -0700

    Task 6 - basedline (N)

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled(
-    [signUpUser(firstName, lastName), uploadPhoto(fileName)],
-  ).then((result) => result).then((value) => value);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return result.map((key) => ({
+    status: key.status,
+    value: key.status === 'fulfilled' ? key.value : String(key.reason),
+  }));
 }
