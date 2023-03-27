08459b60d402587b737dcb2fec773f2f5928c54a
Solomon Kebede <solomonaau188@gmail.com>
2023-03-27 23:07:13 +0000

    Task 11 - eslint fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   const queries = weakMap.get(endpoint) || 0;
-  if (queries >= 5){
+  if (queries >= 5) {
     throw new Error('Endpoint load is high');
   } else {
     weakMap.set(endpoint, queries + 1);
